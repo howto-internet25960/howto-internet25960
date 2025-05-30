6822416297ba06dcde995ef3b07955a7d483bd40
gidado maigari <maigareeg@gmail.com>
2025-05-30 16:02:15 -0700

    Add files via upload

---
+++ python.py
@@ -0,0 +1,8 @@
+-# python3
+quit()
+git clone https//github.com/sundowndev/phoneinfoga
+cd phoneinfoga/
+python3 -m pip install -r requirements.txt
+python3 phoneinfoga.py -v
+python3 phoneinfoga.py -h
+python3 phoneinfoga.py -n
